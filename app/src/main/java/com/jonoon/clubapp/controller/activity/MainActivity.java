10d99824d5f53c3c0a0884731853f307771bf97a
Petrus Han <hrzk_7812345@126.com>
2015-08-06 10:27:20 +0800

    mob

--- app/src/main/java/com/jonoon/clubapp/controller/activity/MainActivity.java
+++ app/src/main/java/com/jonoon/clubapp/controller/activity/MainActivity.java
@@ -25,6 +25,9 @@ import com.jonoon.clubapp.model.constants.ServerUrl;
 import com.jonoon.clubapp.util.L;
 import com.jonoon.clubapp.view.slidingdrawer.WrappingSlidingDrawer;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 public class MainActivity extends BaseActivityWithJavaScriptInterface
         implements NavigationOneFragment.OnFragmentInteractionListener,
         NavigationTwoFragment.OnFragmentInteractionListener{
@@ -93,7 +96,7 @@ public class MainActivity extends BaseActivityWithJavaScriptInterface
 
     @Override
     protected void onDestroy() {
-        L.e(TAG,"onDestroy");
+        L.e(TAG, "onDestroy");
         super.onDestroy();
     }
 
@@ -200,15 +203,45 @@ public class MainActivity extends BaseActivityWithJavaScriptInterface
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
                     break;
                 case 11://VIP
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.fragment_container, MainPageFragment.newInstance(ServerUrl.getVip(), null)
-                            )
-                            .commit();
-
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//                    getSupportFragmentManager().beginTransaction()
+//                            .replace(R.id.fragment_container, MainPageFragment.newInstance(ServerUrl.getVip(), null)
+//                            )
+//                            .commit();
+//
+//                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                    showShare();
             }
         }
         cur_page = page_num;
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+//        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
     }
 
     @Override
